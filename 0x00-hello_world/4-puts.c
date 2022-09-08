59605b8120938b1c627bf3ac1dda78e89069b04d
Tokunbo <tokzy04@gmail.com>
2022-09-08 18:47:13 +0000

    Puts Punction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 /* Here is a scripts that utilizes the puts function to print */
 
-
 #include <stdio.h>
-
-/**                                                                                                                                         *main - print script usint puts function                                                                                                   *                                                                                                                                          *return: Always 0 success                                                                                                                  */ 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-{ 
-
-
+{  
 	puts("\"Programming is like building a multilingual puzzle");
+	 
 	return (0);
-
-
 }
 
