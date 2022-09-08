513a20507bb1035b92b32a5e4335764483dca36c
Tokunbo <tokzy04@gmail.com>
2022-09-08 20:07:55 +0000

    Printf Function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 
-	Printf("with proper grammar, but the outcome is a piece of art\n");
-
-		return(0);
+	Printf("with proper grammar, but the outcome is a piece of art,\n");
+        return(0);
 }
