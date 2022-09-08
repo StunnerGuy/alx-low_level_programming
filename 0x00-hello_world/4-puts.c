4c805ef8bab187349966f9d3ba03f6869a006dab
Tokunbo <tokzy04@gmail.com>
2022-09-08 18:19:14 +0000

    Puts Function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,19 @@
-/*Here is a scripts that utilizes the puts function to print*/
+/* Here is a scripts that utilizes the puts function to print */
 
 #include <stdio.h>
 
-int main(void){ 
-	
+int main(void)
+{ 
+
 /**
- * Return always (0)
+ * Return always (0) success
  *
- * This is a scrip4-puts.cit file that prints using the puts function
+ * This is a script file that prints using the puts function
  */
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
+
 }
+
