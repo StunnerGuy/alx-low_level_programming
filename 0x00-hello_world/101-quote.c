2a32a9dadce6eaf7c29d14c3b0c1b609f08a8251
Tokunbo <tokzy04@gmail.com>
2022-09-08 23:08:31 +0000

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+/* this script prints a string to the stderr */
+
+#include <unistd.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Return: Always 1 (Failed)
+ * Return: Always 1 (failure)
  */
 int main(void)
 {
+		char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	
-	return (1);
+			write(2, str, 59);
+				return (1);
 }
