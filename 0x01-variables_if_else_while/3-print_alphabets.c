ce4a34cfc39f155886ac339e60b07251be733fef
Tokunbo <tokzy04@gmail.com>
2022-09-09 21:04:09 +0000

    uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Display alphabets in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	(ch = 'A', ch <= 'Z', ch++)
+
+		putchar(ch);
+
+		putchar('\n');
+
+	return (0);
+}
