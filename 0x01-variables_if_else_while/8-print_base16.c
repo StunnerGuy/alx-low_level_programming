7ba7e356967ebe651299a3e7460758b396034ec0
Tokunbo <tokzy04@gmail.com>
2022-09-09 23:20:36 +0000

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description - 
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	char ch;
+
+	for (num = 0; num < 10. num++)
+		putchar((num % 10) + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++
+			)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+
+
+}
