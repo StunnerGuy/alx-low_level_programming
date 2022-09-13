2b69ca7abea9b3edaaca9864104072969b33391e
Tokunbo <tokzy04@gmail.com>
2022-09-13 17:19:26 +0000

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A script that prints _putchar
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int a;
+
+	for (a = 0; a < 10; ++a)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is a readme file for Funnctions nested loops.
+0. Write a program that prints _putchar, followed by a new line.
