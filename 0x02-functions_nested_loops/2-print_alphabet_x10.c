56d2508513cc48e55a38b86d2b68f1291136c788
Tokunbo <tokzy04@gmail.com>
2022-09-13 19:56:08 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ * print_alphabet_x10 - A function that will print the alphabet 10 tims
+ *
+ *
+ *
+ */
+
+void print_alphabet_x10(void);
+{
+	int line;
+	char i;
+
+	for (line = 1; line <= 10; line++)
+	{
+		for (i = 'a'; i = 'z'; i++);
+
+		_putchar(ch);
+
+		_putchar('\n')
+}
