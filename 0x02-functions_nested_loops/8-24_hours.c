9baa07736de946f8efb25371d01492d242a95404
Tokunbo <tokzy04@gmail.com>
2022-09-13 23:04:07 +0000

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer- prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int mins, hour, min;
+
+	for (mins = o; mins < (60 * 24); mins++)
+	{
+		hour = mins / 60;
+		mins = mins % 60;
+		_putchar(48 + (hour / 10));
+		_putchar(48 + (hour % 10)):
+		_putchar(':');
+		_putchar(48 + (min / 10));
+		_putchar(48 + (min % 10));
+		_putchar('\n');
+	}
+}
