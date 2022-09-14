482fb71814487f108cb67c62dea5011c4b681f78
Tokunbo <tokzy04@gmail.com>
2022-09-14 01:24:16 +0000

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; ++num)
 		{
-			_putchar(',') ;
+			_putchar(',');
 			for (mult = 1; mult <= n; ++mult)
 			{
 				_putchar(',');
@@ -22,7 +22,7 @@ void print_times_table(int n)
 
 				prod = num * mult;
 
-				if (prod <=9)
+				if (prod <= 9)
 					_putchar(' ');
 				if (prod <= 99)
 					_putchar(' ');
