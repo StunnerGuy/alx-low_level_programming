820f39efeeb0fefc4c9707f8a74e2615b2e560ae
Tokunbo <tokzy04@gmail.com>
2022-09-15 12:28:46 +0000

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h" 
+/**
+ * main - test function that prints integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int 1;
+
+	i = 0;
+	positive or negative (i);
+
+	return (0);
+}
