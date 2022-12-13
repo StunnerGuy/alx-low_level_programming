a869e7f66da6f8c5af0d3f6e5d14354b82fb807c
Tokunbo <tokzy04@gmail.com>
2022-12-13 07:34:01 +0000

    Task Update

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - compute the absolute value of @n
+ *
+ * @n: the input number as an integer
+ *
+ * Return: the absolute value of @n
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
