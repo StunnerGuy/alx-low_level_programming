256f96a269cee02b0e40afc3728a17bd6943f37c
Tokunbo Oshisanya <tokzy04@gmail.com>
2023-10-05 19:17:18 +0100

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*Here is a script that prints into standard output*/
+
+#include <stdio.h>
+
+/**
+*main - Entry Point
+*
+*Return: - Always 0 (Success)
+*Description: Print text and return
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		
+	return (0);
+}
