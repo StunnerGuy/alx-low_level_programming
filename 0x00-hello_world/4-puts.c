e88fdb36d8d3cf1b898a4984f21a9875698ebf38
Tokunbo Oshisanya <tokzy04@gmail.com>
2023-10-05 18:46:28 +0100

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Here is a script that utilizes the puts function to print */
+
+/**
+*main - Entry Point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
