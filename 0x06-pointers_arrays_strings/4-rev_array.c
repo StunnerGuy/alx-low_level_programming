1ac81c36492d537f0910f2fabbeaef2df688d34b
Tokunbo Oshisanya <tokzy04@gmail.com>
2023-10-18 20:29:46 +0100

    Task Update

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverse the contents of an array
+ * @a: array on integers
+ * @n: number of element in the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int middleman;
+	int first = 0;
+	int last = n - 1;
+
+	while (first < last)
+	{
+		/* use middleman to secure a[first]'s value */
+		middleman = a[first];
+		/* replace a[first] with  a[last] */
+		a[first] = a[last];
+		/* replace a[last] with a[first]'s previous value */
+		a[last] = middleman;
+		first++; /* increase first */
+		last--; /* decrease last */
+	}
+}
